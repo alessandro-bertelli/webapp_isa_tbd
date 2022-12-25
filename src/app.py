943f9d19835a3ce9c19845fc755bac4cdd46bb22
Alessandro Bertelli <abertelli98@gmail.com>
2022-12-25 18:31:39 +0100

    [new_pren] First POST requests creation
    Funzionante l'inserimento nel database dei dati sulla tabella cliente e sulla tabella prenotazione, implementata autogenerazione dell'identificativo id_prenotazione. Manca gestione ordine pizze e bevande e gestione coupon.

--- src/app.py
+++ src/app.py
@@ -28,11 +28,26 @@ def index():
 @app.route('/new_pren/', methods=('GET', 'POST'))
 def new_pren():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM pizza;')
-    pizze = cur.fetchall()
-    cur.execute('SELECT * FROM bevanda;')
-    bevande = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('new_pren.html', pizze=pizze, bevande=bevande)
+    if request.method == 'POST':
+        nome = request.form['nome']
+        id_cliente = request.form['id_cliente']
+        telefono = request.form['telefono']
+        data = request.form['data']
+        orario = request.form['orario']
+        cod_coupon = request.form['cod_coupon']
+        cur = conn.cursor()
+        cur.execute('INSERT INTO cliente (nome, id_cliente, telefono)''VALUES (%s, %s, %s)',(nome, id_cliente, telefono))
+        cur.execute('INSERT INTO prenotazione (data, orario, id_cliente)''VALUES (%s, %s, %s)',(data, orario, id_cliente))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+    else:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM pizza;')
+        pizze = cur.fetchall()
+        cur.execute('SELECT * FROM bevanda;')
+        bevande = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template('new_pren.html', pizze=pizze, bevande=bevande)
