5419d51db2661915bb88203f490b43ad4f5f6c98
Alessandro Bertelli <abertelli98@gmail.com>
2022-12-26 19:06:40 +0100

    Create 'del_pren' and 'mod_pren'

--- src/app.py
+++ src/app.py
@@ -51,3 +51,57 @@ def new_pren():
         cur.close()
         conn.close()
         return render_template('new_pren.html', pizze=pizze, bevande=bevande)
+
+@app.route('/mod_pren/', methods=('GET', 'POST'))
+def mod_pren():
+    conn = get_db_connection()
+    if request.method == 'POST':
+        nome = request.form['nome']
+        telefono = request.form['telefono']
+        cod_fiscale = request.form['cod_fiscale']
+        data = request.form['data']
+        orario = request.form['orario']
+        cod_coupon = request.form['cod_coupon']
+        cur = conn.cursor()
+        cur.execute('INSERT INTO cliente (nome, telefono, cod_fiscale)''VALUES (%s, %s, %s)',(nome, telefono, cod_fiscale))
+        cur.execute('INSERT INTO prenotazione (data, orario, cod_fiscale)''VALUES (%s, %s, %s)',(data, orario, cod_fiscale))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+    else:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM pizza;')
+        pizze = cur.fetchall()
+        cur.execute('SELECT * FROM bevanda;')
+        bevande = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template('mod_pren.html', pizze=pizze, bevande=bevande)
+
+@app.route('/del_pren/', methods=('GET', 'POST'))
+def del_pren():
+    conn = get_db_connection()
+    if request.method == 'POST':
+        nome = request.form['nome']
+        telefono = request.form['telefono']
+        cod_fiscale = request.form['cod_fiscale']
+        data = request.form['data']
+        orario = request.form['orario']
+        cod_coupon = request.form['cod_coupon']
+        cur = conn.cursor()
+        cur.execute('INSERT INTO cliente (nome, telefono, cod_fiscale)''VALUES (%s, %s, %s)',(nome, telefono, cod_fiscale))
+        cur.execute('INSERT INTO prenotazione (data, orario, cod_fiscale)''VALUES (%s, %s, %s)',(data, orario, cod_fiscale))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+    else:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM pizza;')
+        pizze = cur.fetchall()
+        cur.execute('SELECT * FROM bevanda;')
+        bevande = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template('del_pren.html', pizze=pizze, bevande=bevande)
